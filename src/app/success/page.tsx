dbd7a8f51adfc3d2c0374f3f413906acd71c9552
Imranullah Afridi <imrulafridi@gmail.com>
2023-08-26 23:37:19 +0500

    Updated

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,11 +1,18 @@
-import { CheckCircle2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { CheckCircle2, ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 
 const Success = () => {
   return (
-    <div className="container mx-auto flex flex-col justify-center items-center h-[60vh] p-8">
-      <CheckCircle2 size={48} color="#4bb95d" strokeWidth={3} />
+    <div className="container mx-auto flex flex-col justify-center items-center h-[60vh] p-8 space-y-4">
+      <CheckCircle2 size={120} color="#4bb95d" strokeWidth={3} />
       <h1 className="font-bold text-4xl uppercase">Success</h1>
+      <Link href={`/products`}>
+          <Button className="m-2 w-56 h-12">
+            <ShoppingCart className="mr-2 h-5 w-5" /> Start Shopping
+          </Button>
+        </Link>
     </div>
   );
 };
